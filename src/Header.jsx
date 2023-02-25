e2f428062bc456498f3022d97c73295a17415314
Sumit Mishra <sumit894sumit@gmail.com>
2023-02-25 16:42:36 +0530

    fixed eslints

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import React from 'react';
 import './App.css';
 
@@ -6,7 +6,8 @@ const Header=()=> {
   return (
   <>
     <div className="header">
-    <img src="https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-notes-business-kiranshastry-lineal-color-kiranshastry.png"/>
+   
+    <img src="https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-notes-business-kiranshastry-lineal-color-kiranshastry.png" alt='impic'/>
     <h1>Sticky Notes</h1>
     </div>
   </>
